d875af81d8aa34c75163304c5dcefef840fe03db
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-06-14 23:55:25 -0300

    chore: config

--- next.config.js
+++ next.config.js
@@ -2,17 +2,15 @@
 
 const environment = process.env.NODE_ENV
 const nextConfig = {
-  assetPrefix: '/next-app/',
-  basePath: '/next-app/',
+  assetPrefix: '/next-app',
+  basePath: '/next-app',
+  // assetPrefix: environment === 'production' ? '/next-app/' : '',
+  // basePath: environment === 'production' ? '/next-app' : '',
   images: {
-    assetPrefix: '/next-app/',
-    basePath: '/next-app/',
-    // assetPrefix: environment === 'production' ? '/next-app/' : '',
-    // basePath: environment === 'production' ? '/next-app' : '',
-    // trailingSlash: true,
     unoptimized: environment === 'production',
   },
   output: environment === 'production' ? 'export' : 'standalone',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
